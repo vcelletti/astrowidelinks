fa52f5ae9e53209645799ed3d2581b2893cb56b5
Jorge Perez <information.jperez@gmail.com>
2025-01-01 12:39:28 +0800

    added deploy actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 import icon from 'astro-icon';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon()],
+  site: 'https://jperez00.github.io',
 });
