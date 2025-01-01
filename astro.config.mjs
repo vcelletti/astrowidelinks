e9e72fb6965f1f68579717080a461205b98dba68
Jorge Perez <information.jperez@gmail.com>
2025-01-01 13:03:49 +0800

    preface img links with repo name for github pages, from /favicon.svg to /astrolinks/favicon.svg

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,5 @@ import icon from 'astro-icon';
 export default defineConfig({
   integrations: [tailwind(), icon()],
   site: 'https://jperez00.github.io/astrolinks',
-  base: '/astrolinks/',
+  base: '/astrolinks',
 });
